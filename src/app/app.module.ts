b6127654e99cfe28b9990cefaf689bc65d1b1953
pedrosnow <pedro6489@gmail.com>
2023-10-05 13:28:32 -0300

    implementando a rota home, diversão, restaurantes e criando o modolo RouteModule'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http';
+
+import { ROUTES } from './app.routes';
 
 import { AppComponent } from './app.component';
+import { DiversaoComponent } from './diversao/diversao.component';
+import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { TopoComponent } from './topo/topo.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from './rodape/rodape.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
     HomeComponent,
-    RodapeComponent
+    RodapeComponent,
+    DiversaoComponent,
+    RestaurantesComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
